3046c53792d2924df202a1b988013ed6ba24c27e
Aplexas99 <toni_dzoic@hotmail.com>
2023-08-25 17:52:38 +0200

    Added all migrations

---
+++ database/migrations/2023_08_25_153408_create_consultation_requests_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('consultation_requests', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('student_id')->constrained()->onDelete('cascade');
+            $table->foreignId('professor_id')->constrained()->onDelete('cascade');
+            $table->foreignId('course_id')->constrained()->onDelete('cascade');
+            $table->foreignId('schedule_id')->constrained()->onDelete('cascade');
+            $table->string('status')->default('PENDING')->enum (['PENDING', 'APPROVED', 'REJECTED']); 
+            $table->text('reason')->nullable();
+            $table->timestamp('start_time')->nullable();
+            $table->timestamp('end_time')->nullable();
+            $table->enum('type', ['IN_PERSON', 'ONLINE'])->default('IN_PERSON');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('consultation_requests');
+    }
+};
