baf7f135cf662485bf657dc20ac9d7305f36d0b9
Aplexas99 <toni_dzoic@hotmail.com>
2023-08-26 13:32:06 +0200

    Added relation

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\ConsultationRequest;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -15,6 +16,10 @@ class Student extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+    public function consultationRequests()
+    {
+        return $this->hasMany(ConsultationRequest::class);
     }
 
 }
